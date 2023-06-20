82c2a7110df19755e7b83854189cbb3628616644
ac4mm <ac4mm@users.noreply.github.com>
2023-06-20 19:04:22 +0200

    refactoring with nf-play-button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
 
+import { NfPlayButton } from '@shared/components/buttons/nf-play-button.component';
 import { NfAddButton } from '@shared/components/buttons/nf-add-button.component';
 import { NfThumbUpButton } from '@shared/components/buttons/nf-thumb-up-button.component';
 import { NfExpandButton } from '@shared/components/buttons/nf-expand-button.component';
@@ -10,9 +11,9 @@ import { LoadingSpinnerComponent } from './components/loading-spinner/loading-sp
 import { DropdownDirective } from './directive/dropdown.directive';
 
 @NgModule({
-  declarations: [NfAddButton, NfThumbUpButton, NfExpandButton, LoadingSpinnerComponent, DropdownDirective, SkeletonCardComponent],
+  declarations: [NfPlayButton, NfAddButton, NfThumbUpButton, NfExpandButton, LoadingSpinnerComponent, DropdownDirective, SkeletonCardComponent],
   imports: [CommonModule, SkeletonModule],
-  exports: [NfAddButton, NfThumbUpButton, NfExpandButton, LoadingSpinnerComponent, DropdownDirective, CommonModule, SkeletonCardComponent],
+  exports: [NfPlayButton, NfAddButton, NfThumbUpButton, NfExpandButton, LoadingSpinnerComponent, DropdownDirective, CommonModule, SkeletonCardComponent],
   providers: [],
 })
 export class SharedModule { }
