6fe0dc5cf5c77b38af5fd475571dc0227df5a0a9
ac4mm <ac4mm@users.noreply.github.com>
2023-07-08 19:22:31 +0200

    fixed imports in module shared

---
+++ src/app/shared/components/buttons/nf-check-button.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'nf-check-button',
+  template: `<button
+                class="btn-circle btn-check btn-icon-cover"
+              >
+              <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="#fff"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="white"
+                  stroke-width="0.3"
+                >
+                  <path
+                    d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z"
+                    fill="#fff"
+                  ></path>
+                </svg>
+              </button>
+  `,
+  styleUrls: ['./nf-buttons.component.scss']
+})
+export class NfCheckButton {
+
+}
