9c962d28363ef601c154cf43902232b1c59ab91a
ac4mm <ac4mm@users.noreply.github.com>
2023-07-29 18:13:43 +0200

    center loading-spinner

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nf-loading-spinner',
-  template: '<div id="loader" class="nfLoader"></div>',
+  template: `<div id="loader" class="nfLoader"></div>`,
   styleUrls: ['./loading-spinner.component.scss'],
 })
-export class LoadingSpinnerComponent {}
+export class LoadingSpinnerComponent {
+}
