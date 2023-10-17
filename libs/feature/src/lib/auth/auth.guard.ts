d1bafc213b1b03a40fd8386829f35fb03a427762
ac4mm <ac4mm@users.noreply.github.com>
2023-10-17 19:52:08 +0200

    linter code

--- libs/feature/src/lib/auth/auth.guard.ts
+++ libs/feature/src/lib/auth/auth.guard.ts
@@ -1,43 +1,21 @@
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-  UrlTree,
-} from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { map, take, takeUntil, tap } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   isAuthenticated = false;
   private destroy$ = new Subject<void>();
-  
-  constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    router: RouterStateSnapshot
-  ):
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate():
     | boolean
     | UrlTree
     | Promise<boolean | UrlTree>
     | Observable<boolean | UrlTree> {
-    /* return this.authService.user.pipe(
-      take(1),
-      map((user) => {
-        const isAuth = !!user;
-        console.log('ISAUTH:' + isAuth);
-        if (isAuth) {
-          return true;
-        } else {
-          console.log('ISAUTH:' + isAuth);
-        }
-        return this.router.createUrlTree(['/login']);
-      }),
-    ); */
     this.authService.user$.pipe(takeUntil(this.destroy$)).subscribe((user) => {
       this.isAuthenticated = !!user;
     });
